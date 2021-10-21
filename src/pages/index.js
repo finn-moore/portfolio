17e3a3a44a9627eabf2e2822d7cc835446bffd30
Finn Moore <finnmoore@gmail.com>
2021-10-20 22:58:44 -0400

    added contact.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Header from '../components/header.js'
 import Projects from '../components/projects.js'
 import About from '../components/about.js'
 import Skills from '../components/skills.js'
+import Contact from '../components/contact.js'
 import Footer from '../components/footer.js'
 import '../styles/global.css'
 
@@ -15,6 +16,7 @@ const Home = () => {
 			<Projects/>
 			<About/>
 			<Skills/>
+			<Contact/>
 			<Footer/>
 		</div>
 	)
